d1ca133b2f559f1bc5a999a5f5a8c02af19f507a
Mu Ye Liu <muyeliu1208@gmail.com>
2025-01-28 18:57:52 -0800

    added mysql

---
+++ backend/src/main/java/muyel/respository/PlayerRepository.java
@@ -0,0 +1,13 @@
+/* 
+package muyel.respository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import muyel.model.Player;
+
+@Repository
+public interface PlayerRepository extends JpaRepository<Player, Long> {
+    Player findByUsername(String username);
+}
+
+*/
